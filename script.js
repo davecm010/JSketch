b7af3cf7d81a68474709fdd6515c054ed44c417c
David Moylan <davecm010@gmail.com>
2017-04-28 13:57:34 -0500

    first draft

---
+++ script.js
@@ -0,0 +1,16 @@
+#container {
+  height: 600px;
+  width: 600px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -300px;
+  margin-top: -300px;
+}
+
+.square {
+  border: solid black 1px;
+  height: auto;
+  width: auto;
+  float: left;
+}
