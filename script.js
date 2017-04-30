6e6370209f7606a0c90a3de4352ceb510e07e5ac
davecm010 <davecm010@gmail.com>
2017-04-30 13:47:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-button {
-  padding: 10px 20px;
-  border-radius: 4px;
-  }
+var dim = 16
 
-#container {
-  height: 640px;
-  width: 640px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  margin-left: -320px;
-  margin-top: -320px;
-  background-color: white;
+$(document).ready(function() {
+
+  make_grid(dim)
+
+  $('button').click(function() {
+    dim = prompt("Change Dimensions", "Enter a number between 1-64");
+    $('.square').remove();
+    make_grid(check_value(dim))
+  })
+});
+
+function check_value(x) {
+  if (isNaN(x)) {
+    alert("This is not a number");
+  } else if (x < 1) {
+    return 1;
+  } else if (x > 64) {
+  	return 64;
+  } else {
+  	return x;
+  }
 }
 
-.square {
-  height: 40px;
-  width: 40px;
-  float: left;
+function make_grid(dim) {
+  for (var x = 0; x < dim; x++) {
+    for (var y = 0; y < dim; y++) {
+      var size = (640 / dim);
+      var square = $("<div class='square'></div>");
+      square.height(size);
+      square.width(size);
+      square.appendTo('#container');
+      (square).hover(function() {
+        $(this).css("background-color", "grey");
+      }, function() {
+        $(this).css("background-color", "black");
+      })
+    }
+  }
 }
